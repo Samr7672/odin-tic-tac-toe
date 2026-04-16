5a7a496974cb562a93a1a699108136c03c214118
samr7672 <shivamkumar3214rew@gmail.com>
2026-04-17 04:11:22 +0530

    finalize css and html

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-const gameboard=[];
+const start=document.querySelector(".submit")
+const reset=document.querySelector(".reset")
 
-const player=[];
+reset.addEventListener("click",()=>{
+    console.log("restarting the game ......")
+    const squares=document.querySelectorAll(".board");
+    squares.forEach(square => {
+
+    square.textContent="";  
+    });
+})
+
+start.addEventListener("click",()=>{
+    console.log("hello world");
+    const container=document.querySelector(".container");
+    container.style.display="grid";
+})
